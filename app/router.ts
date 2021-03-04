457616ae81fde83c5c6853419a5cc8bbd3f7af42
lss <1124965100@qq.com>
2021-03-04 11:15:59 +0800

    2021/3/4

--- app/router.ts
+++ app/router.ts
@@ -4,6 +4,6 @@ export default (app: Application) => {
   const { controller, router } = app;
 
   router.get('/', controller.home.index);
-  router.get('/github/callback', controller.user.githubLogin)
+  router.get('/github/callback', controller.user.githubLogin);
 };
  
